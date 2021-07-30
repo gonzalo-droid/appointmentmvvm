3946a6ee42bf106e996b00803b783d3b2d6dd87d
yodaRoot <gonzalolopezguerrero2050@gmail.com>
2021-07-30 15:17:49 -0500

    v1 mvvm

---
+++ app/src/main/java/com/example/appointmentmvvm/model/QuoteModel.kt
@@ -0,0 +1,3 @@
+package com.example.appointmentmvvm.model
+
+data class QuoteModel (val quote:String , val author:String)
---
+++ app/src/main/java/com/example/appointmentmvvm/view/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.appointmentmvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.example.appointmentmvvm.databinding.ActivityMainBinding
+import com.example.appointmentmvvm.viewmodel.QuoteViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    // implementation "androidx.activity:activity-ktx:1.2.2"
+    //conexion del viewmodel al activity
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
+            binding.tvQuote.text = currentQuote.quote
+            binding.tvAuthor.text = currentQuote.author
+        })
+
+        binding.viewContainer.setOnClickListener {
+            quoteViewModel.randomQuote()
+        }
+    }
+}
